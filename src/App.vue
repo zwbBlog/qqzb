76d41fdc7ef88a10a6f897a347137a057e0e3d3e
zhang <zhang_wenbin@aliyun.com>
2018-03-29 11:41:08 +0800

    兼容苹果设备横竖屏幕切换播放

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <!-- <span class="qqzb_icon-guanbi guanbi" style="font-size:26px"></span> -->
     </div>
     <div id="video_content">
-        <ali-player @play="played" :source="aplayer.source" :vid="aplayer.vid" :playauth="aplayer.playauth" ref="player"></ali-player>
+        <ali-player @play="played" :source="aplayer.source" :vid="aplayer.vid" :playauth="aplayer.playauth" ref="player" :height="aplayer.height"></ali-player>
         <p class="renshu">
           <span class="qqzb_icon-renshu"></span>
           <span class="subTitle">{{renshu}}人在看</span>
@@ -58,7 +58,8 @@ export default {
       renshu: 0,
       vmode: false,
       aplayer: {
-        source: "./assets/media/80test.mp4",
+        height:"100%",
+        source: "../static/media/80test.mp4",
         vid: "8db6e5c7ff5f4257b41e2487ec61d592",
         playauth:
           "eyJTZWN1cml0eVRva2VuIjoiQ0FJUzN3SjFxNkZ0NUIyeWZTaklxYXIzQm8zVm9lZGlqNWU1YW12OGswY3hPYmREaDdEQXRqejJJSHBLZVhkdUFlQVhzL28wbW1oWjcvWVlsck1xRnNFYkZCMlpNcHNwc2M4Sm9GUCtKcExGc3QySjZyOEpqc1ZsMDRWdmxsMnBzdlhKYXNEVkVma3VFNVhFTWlJNS8wMGU2TC8rY2lyWVhEN0JHSmFWaUpsaFE4MEtWdzJqRjFSdkQ4dFhJUTBRazYxOUszemRaOW1nTGlidWkzdnhDa1J2MkhCaWptOHR4cW1qL015UTV4MzFpMXYweStCM3dZSHRPY3FjYThCOU1ZMVdUc3Uxdm9oemFyR1Q2Q3BaK2psTStxQVU2cWxZNG1YcnM5cUhFa0ZOd0JpWFNaMjJsT2RpTndoa2ZLTTNOcmRacGZ6bjc1MUN0L2ZVaXA3OHhtUW1YNGdYY1Z5R0ZOLzducFNVUmJ2M2I0eGxMZXVrQVJtWGpJRFRiS3VTbWhnL2ZIY1dPRGxOZjljY01YSnFBWFF1TUdxQWRQTDVwZ2lhTTFyOUVQYmRnZmhtaTRBSjVsSHA3TWVNR1YrRGVMeVF5aDBFSWFVN2EwNDR4ckRoRzVnS3NNUWFnQUY2Qzl6eHF3VmdsSEFzTTJXdm11cmk1bFFRUHlYMXdndmJ4Mk91d3BRQmF0SEYrdW8wUnNURzllb2tVZmRMS0lpbkxsYlIvKys1MzlXN3BQM0k0T3h3ZjZIV2FjQTZQZVM2ZmpWQnZhd3BWSE5oNHdYR20rYk8xdUdtTXRraEhiMkExOTZUbEtYT0I4ZStEZUZ5Q1VpcVlrb21KaXdhTytGa0cxOW55SlVBUHc9PSIsIkF1dGhJbmZvIjoie1wiQ2FsbGVyXCI6XCJiY0o0ZFJHRFdRTjJnTEFvTk9nbVpwS0FMNEIxSk55YmNxaThOWkZCOERvPVxcclxcblwiLFwiRXhwaXJlVGltZVwiOlwiMjAxNy0xMC0yNVQwNjo1Mzo0N1pcIixcIk1lZGlhSWRcIjpcIjhkYjZlNWM3ZmY1ZjQyNTdiNDFlMjQ4N2VjNjFkNTkyXCIsXCJTaWduYXR1cmVcIjpcIlFHNTcwTXlrQWxzN1pxS1dTd2ppVStseTJrQT1cIn0iLCJWaWRlb01ldGEiOnsiU3RhdHVzIjoiTm9ybWFsIiwiVmlkZW9JZCI6IjhkYjZlNWM3ZmY1ZjQyNTdiNDFlMjQ4N2VjNjFkNTkyIiwiVGl0bGUiOiJXZUNoYXRfMjAxNzEwMTYxNjMzMTkiLCJDb3ZlclVSTCI6Imh0dHA6Ly92b2QubHVvYm9iYi5jb20vc25hcHNob3QvOGRiNmU1YzdmZjVmNDI1N2I0MWUyNDg3ZWM2MWQ1OTIwMDAwMS5qcGciLCJEdXJhdGlvbiI6NDIwLjB9LCJBY2Nlc3NLZXlJZCI6IlNUUy5IaUJNN2FMOEM4VVJoTU13R2Q1OG9odWtUIiwiUGxheURvbWFpbiI6InZvZC5sdW9ib2JiLmNvbSIsIkFjY2Vzc0tleVNlY3JldCI6IkRoZVdWNmJEMVBvN2pQbXJ1TFRDc25kQTFCUE5VTmpOb29iRnVEWnRqaDNlIiwiUmVnaW9uIjoiY24tc2hhbmdoYWkiLCJDdXN0b21lcklkIjoxNTY2OTk1NzgxNzM2OTg3fQ=="
@@ -100,18 +101,19 @@ export default {
     var self=this;
     (function() {
       var updateOrientation = function() {
-        var orientation =
-          window.innerWidth > window.innerHeight ? "landscape" : "portrait";
+        var orientation = window.innerWidth > window.innerHeight ? "landscape" : "portrait";
         // document.body.parentNode.setAttribute('class', orientation);
         console.log(orientation);
         if (orientation == "portrait") {
           //竖屏
           console.log('竖屏');
-          self.vmode=false
+          self.vmode=false;
+          document.getElementById('video_content').style.height=4.9+'rem';
         } else {
           //横屏
           console.log('横屏');
-          self.vmode=true
+          self.vmode=true;
+          document.getElementById('video_content').style.height="100%";
         }
       };
       var init = function() {
@@ -179,7 +181,7 @@ body,
   margin: 0 auto;
 }
 .mint-navbar .mint-tab-item.is-selected {
-  border: none;
+  border: none !important;
 }
 .mint-tab-item.is-selected .mint-tab-item-label {
   border-bottom: 3px solid rgb(254, 102, 18);
@@ -239,7 +241,7 @@ body,
   text-align: center;
   line-height: 0.5rem;
   color: #ccc;
-  font-size: 0.45rem;
+  font-size: 0.45rem !important;
 }
 
 .loginModal .mint-cell-wrapper {
